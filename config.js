ef5ea9adf71b296a5afde47eec279e2c7c0a886a
Hemanthchinnu <41671666+Hemanthchinnu@users.noreply.github.com>
2018-12-29 12:13:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    endpoint: 'https://ropsten.infura.io/v3/49ec18cc3e22418f9c8901d695d68f44',
+    from: '0xDc0ba17b3FD674F8372df48B84CB6b75f5451C0B',
+    to: '0x7A03C24D58A19D27A66dBa7769573A0E7FDF3989',
+    privkey: 'F1B231303267FCFD236E279BA712CD457E5D917871FD25A414107618BFAD8120',
+    gasPrice: 3000000000,
+    gasLimit: 21000
+}
+
+module.exports = config;
